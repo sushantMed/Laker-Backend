24a07bfedd945d4f87667ee9d267161339917856
sushant.sinha <sushant.sinha@businessonetech.com>
2026-06-18 04:37:18 +0000

    fixed migration and table

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,13 +3,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database.base import Base
 from uuid import uuid4, UUID
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+
 
 
 #we can skip this because we are not following
 class UserModel(Base):
     __tablename__ = "users"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, default=uuid4)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
     first_name: Mapped[str] = mapped_column(String(100), nullable=False)
     last_name: Mapped[str] = mapped_column(String(100), nullable=False)
